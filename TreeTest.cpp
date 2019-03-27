fa392d74125c149c5702e6ed1674989da350e8fc
orel <orel.rahum@gmail.com>
2019-03-27 17:45:04 +0200

    commit

--- TreeTest.cpp
+++ TreeTest.cpp
@@ -26,19 +26,19 @@ int main() {
   .CHECK_EQUAL (emptytree.size(), 1)
   .CHECK_EQUAL (emptytree.contains(5), true)
   .CHECK_OK    (emptytree.remove(5))
-  // .CHECK_EQUAL (emptytree.contains(5), false)
-  // // .CHECK_THROWS(emptytree.remove(5))
-  // // .CHECK_EQUAL (emptytree.size() ,0)
+  .CHECK_EQUAL (emptytree.contains(5), false)
+  .CHECK_THROWS(emptytree.remove(5))
+  .CHECK_EQUAL (emptytree.size() ,0)
   
-  // .CHECK_EQUAL (threetree.size(), 3)
-  // .CHECK_EQUAL (threetree.root(), 5)
-  // .CHECK_EQUAL (threetree.parent(3), 5)
-  // .CHECK_EQUAL (threetree.parent(7), 5)
+  .CHECK_EQUAL (threetree.size(), 3)
+  .CHECK_EQUAL (threetree.root(), 5)
+  .CHECK_EQUAL (threetree.parent(3), 5)
+  .CHECK_EQUAL (threetree.parent(7), 5)
   .CHECK_EQUAL (threetree.left(5), 3)
   .CHECK_EQUAL (threetree.right(5), 7)
   .CHECK_THROWS(threetree.insert(3))
   .CHECK_THROWS(threetree.left(6))
-  // // .CHECK_OK    (threetree.print())
+  .CHECK_OK    (threetree.print())
   .print();
   
   cout << "You have " << tc.right() << " right answers and " << tc.wrong() << " wrong answers so your grade is " << tc.grade() << ". Great!" << endl;
