3f31c3a765cbf9dff8f6e162f85dcb2312979e96
orel <orel.rahum@gmail.com>
2019-03-29 23:28:40 +0300

    a

--- Tree.cpp
+++ Tree.cpp
@@ -114,12 +114,12 @@ int Tree::right(int i){
         else return _root->_right->_root->getValue();
     }
     else if(_root->getValue() < i) {
-        if (_root->_right!=NULL) {return _root->_right->right(i);}
+        if (_root->_right==NULL){throw std::exception();} 
+        else return _root->_right->right(i);}
     else {
-        if (_root->_left!=NULL){
-            if (_root->_left!=NULL){return _root->_left->right(i);}}}
-    }
-return -1;
+        if (_root->_left==NULL){throw std::exception();}
+        else return _root->_left->right(i);}
+        return -1;
 }
 int Tree::left(int i){
     if(!Tree::contains(i)) {throw std::exception();}
@@ -128,9 +128,12 @@ int Tree::left(int i){
         else return _root->_left->_root->getValue();
     }
     else if(_root->getValue() < i) {
-        if (_root->_right!=NULL){return _root->_right->left(i);}}
+        if (_root->_right==NULL){throw std::exception();}
+        else return _root->_right->left(i);
+        }
     else {
-        if (_root->_left!=NULL)  {return _root->_left->left(i);}}
+        if (_root->_left==NULL){throw std::exception();}
+        else return _root->_left->left(i);}
     return -1;
 }
 
