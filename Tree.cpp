91a7f481889dcb2da55f4cd0d62a5d4db610a307
orelrahum <orel.rahum@gmail.com>
2019-03-31 04:32:21 +0300

    fix

--- Tree.cpp
+++ Tree.cpp
@@ -141,12 +141,12 @@ int Tree::right(int i){
         else return _root->_right->_root->getValue();
     }
     else if(_root->getValue() < i) {
-        if (_root->_right!=NULL) {throw std::exception();}
-        else {return _root->_right->right(i);}
+        if (_root->_right==NULL) {throw std::exception();}
+        else { _root->_right->right(i);}
     }
     else {
-        if (_root->_left!=NULL){throw std::exception();}
-        else {return _root->_left->right(i);}
+        if (_root->_left==NULL){throw std::exception();}
+        else { _root->_left->right(i);}
         }
 return -1;
 }
