b781478188211c12dcbe95257ef1da69e4bd4b57
orelrahum <orel.rahum@gmail.com>
2019-03-31 04:14:31 +0300

    fix

--- Tree.cpp
+++ Tree.cpp
@@ -140,30 +140,30 @@ int Tree::right(int i){
         if (_root->_right->_root==NULL){throw std::exception();}
         else return _root->_right->_root->getValue();
     }
-    // else if(_root->getValue() < i) {
-    //     if (_root->_right!=NULL) {throw std::exception();}
-    //     else {return _root->_right->right(i);}
-    // }
-    // else {
-    //     if (_root->_left!=NULL){throw std::exception();}
-    //     else {return _root->_left->right(i);}
-    //     }
+    else if(_root->getValue() < i) {
+        if (_root->_right!=NULL) {throw std::exception();}
+        else {return _root->_right->right(i);}
+    }
+    else {
+        if (_root->_left!=NULL){throw std::exception();}
+        else {return _root->_left->right(i);}
+        }
 return -1;
 }
  int Tree::left(int i){
-    if(!contains(i)) {throw std::exception();}
+    if(!Tree::contains(i)) {throw std::exception();}
     if(_root->getValue()==i){
         if (_root->_left->_root==NULL){throw std::exception();}
         else return _root->_left->_root->getValue();
     }
     else if(_root->getValue() < i) {
-        if (_root->_left->_root==NULL) {throw std::exception();}
+        if (_root->_right->_root!=NULL) {throw std::exception();}
         else {return _root->_right->left(i);}
     }
-    // else {
-    //     if (_root->_left->_root!=NULL){throw std::exception();}
-    //     else {return _root->_left->left(i);}
-    //     }
+    else {
+        if (_root->_left->_root!=NULL){throw std::exception();}
+        else {return _root->_left->left(i);}
+        }
       return -1;
  }
 
